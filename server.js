ae71a38c54e79dfcbfc75551371070f987e4d03a
Mohamed <mohamedben0203@gmail.com>
2026-06-10 19:45:51 +0200

    Set defaullt port to 3000

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/reports", reportRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
